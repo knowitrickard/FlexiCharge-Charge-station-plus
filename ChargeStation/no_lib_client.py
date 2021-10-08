3dd47940dc87075b3af5531f3d0f9960d5b6b526
Andreas Grahn <andreas.grahn@live.com>
2021-10-08 12:15:13 +0200

    Boot working

--- ChargeStation/no_lib_client.py
+++ ChargeStation/no_lib_client.py
@@ -4,10 +4,6 @@ import websockets
 from datetime import datetime
 import json
 import asyncio
-import concurrent.futures
-import sys
-import time
-import threading
 
 class ChargePoint():
     my_websocket = None
@@ -15,29 +11,6 @@ class ChargePoint():
     def __init__(self, id, connection):
         self.my_websocket = connection
         self.my_id = id
-        _thread = threading.Thread(target=self.between_callback, args=("some text"))
-        _thread.start()
-            
-
-    async def some_callback(args):
-        await 
-
-    def between_callback(args):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        loop.run_until_complete(self.some_callback(args))
-        loop.close()
-
-    _thread = threading.Thread(target=between_callback, args=("some text"))
-    _thread.start()
-
-
-
-    async def check_for_message(self):
-        while 1:
-            await self.my_websocket.recv()
-            asyncio.sleep(2)
 
     async def send_boot_notification(self):
         msg = [2, "0jdsEnnyo2kpCP8FLfHlNpbvQXosR5ZNlh8v", "BootNotification", {
@@ -52,20 +25,14 @@ class ChargePoint():
             "meterSerialNumber": "avt.001.13.1.01" }]
         msg_send = json.dumps(msg)
         await self.my_websocket.send(msg_send)
-        #response = await self.my_websocket.recv()
-        #print(json.loads(response))
-        #await asyncio.sleep(1)
+        response = await self.my_websocket.recv()
+        print(json.loads(response))
+        await asyncio.sleep(1)
 
     async def send_data_transfer_req(self):
         msg = [1]
         msg_send = json.dumps(msg)
         await self.my_websocket.send(msg_send)
-        #response = await self.my_websocket.recv()
-        #print(json.loads(response))
-        #await asyncio.sleep(1)
-
-
-
 
 async def user_input_task(cp):
     while 1:
@@ -75,15 +42,15 @@ async def user_input_task(cp):
             await asyncio.gather(cp.send_boot_notification())
         elif a == 9:
             await asyncio.sleep(2)
-    
+
 async def main():
     async with websockets.connect(
         'ws://54.220.194.65:1337/chargerplus',
          subprotocols=['ocpp1.6']
     ) as ws:
         chargePoint = ChargePoint("chargerplus", ws)
+
         await chargePoint.send_boot_notification()
-        await chargePoint.check_for_message()
         await user_input_task(chargePoint)
 
 if __name__ == '__main__':
