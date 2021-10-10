cd149276a3940cd0f6d247789176063bbcbcbba6
Andreas Grahn <andreas.grahn@live.com>
2021-10-10 09:39:07 +0200

    Multiple reserve now changes and added remote start

--- ChargeStation/no_lib_client.py
+++ ChargeStation/no_lib_client.py
@@ -12,7 +12,15 @@ class ChargePoint():
     my_websocket = None
     my_id = ""
 
+    #Reservation related variables
     reserve_now_timer = 0
+    is_reserved = False
+    reservation_id_tag = None
+    reservation_id = None
+    reserved_connector = None
+
+    #Transaction related variables
+    is_charging = False
 
     #Define enums for status and error_code (or use the onses in OCPP library)
     status = "Available"
@@ -23,71 +31,111 @@ class ChargePoint():
 
     transaction_id = 123
 
-    #Wont't be used in the final product. It is here until back-end is done with thier implementation (then we know what to send instead)
-    id_tag = 123456
-
     charger_id = 00000
 
     timestamp_at_last_heartbeat : float = time.perf_counter()
     time_between_heartbeats = 60 * 60 * 24 #In seconds (heartbeat should be sent once every 24h)
+
 
     def __init__(self, id, connection):
         self.my_websocket = connection
         self.my_id = id
 
     async def get_message(self):
-        print("Check messages")
         try:
             msg = await asyncio.wait_for(self.my_websocket.recv(), 1)
             #async for msg in self.my_websocket: #Takes latest message
             print("Check for message")
             message = json.loads(msg)
-            print(message)
+            #print(message)
 
             if message[2] == "ReserveNow":
                 await asyncio.gather(self.reserve_now(message))
             elif message[2] == "BootNotification":
                 self.charger_id = message[3]["chargerId"] #This is the id number we get from the server (100001)
                 print(self.charger_id)
-            elif message[2] == "RemoteStart":
-                await asyncio.gather(self.remote_start_transaction(message[1]))
+            elif message[2] == "RemoteStartTransaction":
+                await asyncio.gather(self.remote_start_transaction(message))
         except:
             pass
 
-
-    async def remote_start_transaction(self, unique_message_id):
-        msg = [3, 
-            unique_message_id, 
-            "RemoteStartTransaction", 
-            {"status": "Accepted"}
-        ]
-        response = json.dumps(msg)
-        await self.my_websocket.send(response)
+    #AuthorizeRemoteTxRequests is always false since no authorize function exists in backend(?)
+    #TODO - Change when multiple connectors exists. Add parent id tag.
+    async def remote_start_transaction(self, message):
+        if int(message[3]["idTag"]) == self.reservation_id_tag: #If the idTag has a reservation
+            is_charging = True
+            print("Remote transaction started")
+            msg = [3, 
+                message[1], #Unique message id
+                "RemoteStartTransaction", 
+                {"status": "Accepted"}
+            ]
+            response = json.dumps(msg)
+            await self.my_websocket.send(response)
+        else:   #A non reserved tag tries to use the connector
+            print("This tag does not have a reservation")
+            msg = [3, 
+                message[1], #Unique message id
+                "RemoteStartTransaction", 
+                {"status": "Rejected"}
+            ]
+            response = json.dumps(msg)
+            await self.my_websocket.send(response)
 
     #Will count down every second
     def timer_countdown_reservation(self):
         if self.reserve_now_timer <= 0:
             print("Reservation is up!")
+            self.reset_reservation()
             return
         self.reserve_now_timer = self.reserve_now_timer - 1
         print(self.reserve_now_timer)
-        threading.Timer(1, self.timer_countdown_reservation).start()
+        threading.Timer(1, self.timer_countdown_reservation).start()    #Countdown every second
+    
+    def reset_reservation(self):
+        self.is_reserved = False
+        self.reserve_now_timer = 0
+        self.reservation_id_tag = None
+        self.reservation_id = None
 
 
     async def reserve_now(self, message):
-        timestamp = message[3]["expiryDate"]   #Given in ms since epoch
-        reserved_for_ms = int(timestamp - (int(time.time()*1000)))
-        self.reserve_now_timer = int(reserved_for_ms/100)   #This should be changed to seconds. Time received is too short to test
+        if self.reservation_id == None or self.reservation_id == message[3]["reservationID"]:
+            self.reset_reservation()
+            self.is_reserved = True
+            self.reservation_id_tag = message[3]["idTag"]
+            self.reservation_id = message[3]["reservationID"]
+            self.reserved_connector = message[3]["connectorID"]
+            timestamp = message[3]["expiryDate"]   #Given in ms since epoch
+            reserved_for_ms = int(timestamp - (int(time.time()*1000)))
+            self.reserve_now_timer = int(10)#reserved_for_ms/1000)   #Reservation time in seconds
+            threading.Timer(1, self.timer_countdown_reservation).start()    #Countdown every second
 
-        threading.Timer(1, self.timer_countdown_reservation).start()
-
-        msg = [3, 
-            message[1], #Have to use the unique message id received from server
-            "ReserveNow", 
-            {"status": "Accepted"}
-        ]
-        msg_send = json.dumps(msg)
-        await self.my_websocket.send(msg_send)
+            msg = [3, 
+                message[1], #Have to use the unique message id received from server
+                "ReserveNow", 
+                {"status": "Accepted"}
+            ]
+            msg_send = json.dumps(msg)
+            await self.my_websocket.send(msg_send)
+        elif self.reserved_connector == message[3]["connectorID"]:
+            print("Connector occupied")
+            msg = [3, 
+                message[1], #Have to use the unique message id received from server
+                "ReserveNow", 
+                {"status": "Occupied"}
+            ]
+            msg_send = json.dumps(msg)
+            await self.my_websocket.send(msg_send)
+        else:
+            print("Implement other messages for non accepted reservations")
+            msg = [3, 
+                message[1], #Have to use the unique message id received from server
+                "ReserveNow", 
+                {"status": "Occupied"}
+            ]
+            msg_send = json.dumps(msg)
+            await self.my_websocket.send(msg_send)
 
     async def send_boot_notification(self):
         msg = [2, "0jdsEnnyo2kpCP8FLfHlNpbvQXosR5ZNlh8v", "BootNotification", {
@@ -104,15 +152,7 @@ class ChargePoint():
         await self.my_websocket.send(msg_send)
         await asyncio.sleep(1)
 
-    async def send_data_transfer_req(self):
-        msg = [1]
-        msg_send = json.dumps(msg)
-        await self.my_websocket.send(msg_send)
-        #response = await self.my_websocket.recv()
-        #print(json.loads(response))
-        #await asyncio.sleep(1)
-
-    #Gets no response, is this an error in back-end? Seems to be the case (Update: No response seems to be expected)
+    #Gets no response, is this an error in back-end? Seems to be the case
     async def send_status_notification(self):
         current_time = datetime.now()
         timestamp = current_time.timestamp() #Can be removed if back-end does want the time-stamp formated
@@ -130,11 +170,10 @@ class ChargePoint():
 
         msg_send = json.dumps(msg)
         await self.my_websocket.send(msg_send)
-        #No response expected
-        #response = await self.my_websocket.recv()
-        #print(json.loads(response))
+        response = await self.my_websocket.recv()
+        print(json.loads(response))
 
-    #Depricated in backend
+    #Not tested yet, back-end has no implementation for heartbeat at the moment
     async def send_heartbeat(self):
         msg = [2, "0jdsEnnyo2kpCP8FLfHlNpbvQXosR5ZNlh8v", "Heartbeat", {}]
         msg_send = json.dumps(msg)
@@ -150,7 +189,6 @@ class ChargePoint():
         else:
             return False
 
-    #Depricated in back-end
     async def send_meter_values(self):
         current_time = datetime.now()
         timestamp = current_time.timestamp() #Can be removed if back-end does want the time-stamp formated
@@ -188,57 +226,34 @@ class ChargePoint():
         print(json.loads(response))
         await asyncio.sleep(1)
 
-    #This is a test function to initiate communication from server side
-    async def send_data_transfer_req(self):
+
+
+
+
+
+
+
+
+
+
+    async def send_data_reserve(self):
         msg = ["chargerplus", "ReserveNow"]
         msg_send = json.dumps(msg)
         await self.my_websocket.send(msg_send)
-
-    #Will need changes when back-end is done!
-    async def stop_transaction(self):
-        meter_stop = 123
-        reason = "Remote"
-
-        current_time = datetime.now()
-        timestamp = current_time.timestamp() #Can be removed if back-end does want the time-stamp formated
-        formated_timestamp = current_time.strftime("%Y-%m-%dT%H:%M:%SZ") #Can be removed if back-end does not want the time-stamp formated
-        
-        #Back-end won't use samapled values, instead (I beleave) that they only want the charged level
-        sample_value = "12345"
-        sample_context = "Sample.Clock"
-        sample_format = "Raw"
-        sample_measurand = "Energy.Active.Export.Register"
-        sample_phase = "L1"
-        sample_location = "Cable"
-        sample_unit = "kWh"
-
-        msg = [2, "0jdsEnnyo2kpCP8FLfHlNpbvQXosR5ZNlh8v", "StopTransaction",{
-                "idTag" : self.id_tag,
-                "meterStop" : meter_stop,
-                "timestamp" : formated_timestamp,
-                "transactionId" : self.transaction_id,
-                "reason" : reason,
-                "transactionData" : [{
-                    "timestamp": formated_timestamp,
-                    "sampledValue":[
-                        {"value" : sample_value,
-                        "context" : sample_context,
-                        "format" : sample_format,
-                        "measurand": sample_measurand,
-                        "phase": sample_phase,
-                        "location" : sample_location,
-                        "unit": sample_unit},
-                        ]
-                    },],
-        }]
-
+    async def send_data_remote_start(self):
+        msg = ["chargerplus", "RemoteStart"]
         msg_send = json.dumps(msg)
         await self.my_websocket.send(msg_send)
-        response = await self.my_websocket.recv()
-        response_parsed = json.loads(response)
-        stop_status = response_parsed[3]['status']
-        print("Stap status: " + stop_status)
-        await asyncio.sleep(1)
+
+
+
+
+
+
+
+
+
+
 
 async def user_input_task(cp):
     while 1:
@@ -250,7 +265,6 @@ async def user_input_task(cp):
             print("Heartbeat")
 
         a = int(input(">> "))
-
         if a == 1:
             print("Testing boot notification")
             await asyncio.gather(cp.send_boot_notification())
@@ -265,7 +279,13 @@ async def user_input_task(cp):
             await asyncio.gather(cp.send_meter_values())
         elif a == 5:
             print("Testing reserve now")
-            await asyncio.gather(cp.send_data_transfer_req())
+            await asyncio.gather(cp.send_data_reserve())
+        elif a == 6:
+            print("Testing remote start")
+            await asyncio.gather(cp.send_data_remote_start())
+        elif a == 7:
+            print("Reset reservation")
+            cp.reset_reservation()
         elif a == 9:
             await asyncio.sleep(0.1)
 
